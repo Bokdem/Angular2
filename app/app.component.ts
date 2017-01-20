9c3cbc95849a3305aad5c0e1c7481eb61d6e9d79
Bokdem <ikheetmathijs@hotmail.com>
2017-01-20 19:56:06 +0100

    added data + toggle function

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,19 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  	<h1>Naam: {{name}}</h1>
-  	<p><strong>Email:</strong> {{email}}</p>
-  	<p><strong> Adres:</strong>{{address.street}}, {{address.city}}, {{address.state}}</p>
-  	`,
+  template: `<user></user>`,
 })
 
 export class AppComponent  { 
-	name = 'Mathijs de Bok',
-	email = 'mathijs@gmail.com',
-	address = {
-		street: 'Hester Hoofterf 22',
-		city: 'Apeldoorn',
-		state: 'Gelderland'
-	}
+
 }
---
+++ app/components/user.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user',
+  template: `
+  	<h1>Naam: {{name}}</h1>
+  	<p><strong>Email:</strong> {{email}}</p>
+  	<p><strong>Adres:</strong> {{address.street}}, {{address.city}}, {{address.state}}</p>
+  	<button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+  	<div *ngIf="showHobbies">
+	  	<h3>Hobbies: </h3>
+	  	<ul>
+	  		<li *ngFor="let hobby of hobbies">{{hobby}}</li>
+	  	</ul>
+	</div>
+  	`,
+})
+
+export class UserComponent  { 
+	name: string;
+	email: string;
+	address: address;
+	hobbies: strin[];
+	showHobbies: boolean;
+
+	constructor() {
+		this.name = 'Mathijs de Bok',
+		this.email = 'mathijs@gmail.com',
+		this.address = {
+			street: 'Hester Hoofterf 22',
+			city: 'Apeldoorn',
+			state: 'Gelderland'
+		}
+		this.hobbies = ['Music', 'Movies', 'Sports'],
+		this.showHobbies = false;
+	}
+
+	toggleHobbies() {
+		if(this.showHobbies == true) {
+			this.showHobbies = false;
+		} else {
+		this.showHobbies = true;
+	}
+}
+
+interface address {
+	street: string;
+	city: string;
+	state: string;
+}
