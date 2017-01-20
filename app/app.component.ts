759978af8bb012bffddfb162c20663c1af22df47
Bokdem <ikheetmathijs@hotmail.com>
2017-01-20 19:31:55 +0100

    added some data

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  	<h1>Naam: {{name}}</h1>
+  	<p><strong>Email:</strong> {{email}}</p>
+  	<p><strong> Adres:</strong>{{address.street}}, {{address.city}}, {{address.state}}</p>
+  	`,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+	name = 'Mathijs de Bok',
+	email = 'mathijs@gmail.com',
+	address = {
+		street: 'Hester Hoofterf 22',
+		city: 'Apeldoorn',
+		state: 'Gelderland'
+	}
+}
